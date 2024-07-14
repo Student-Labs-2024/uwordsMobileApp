ea9e65d937a33dc89f6ed9ce3b8a64dec161fd54
Nailloon <artemfilyakin5@gmail.com>
2024-07-14 11:10:26 +0600

    [refactor] Removed unused imports

--- lib/features/database/data_sources/savable_user_data_source.dart
+++ lib/features/database/data_sources/savable_user_data_source.dart
@@ -1,5 +1,4 @@
 import 'package:drift/drift.dart';
-import 'package:flutter/foundation.dart';
 import 'package:uwords/features/auth/domain/user_auth_dto.dart';
 import 'package:uwords/features/auth/domain/user_auth_mapper.dart';
 import 'package:uwords/features/database/uwords_database/uwords_database.dart';
