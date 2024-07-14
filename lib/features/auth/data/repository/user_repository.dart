8850f3dca9f53ef629f5a09a542995530820bddc
Nailloon <artemfilyakin5@gmail.com>
2024-07-14 11:10:26 +0600

    [refactor] Removed unused imports

--- lib/features/auth/data/repository/user_repository.dart
+++ lib/features/auth/data/repository/user_repository.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
-import 'package:uwords/common/exceptions/login_exceptions.dart';
 import 'package:uwords/features/auth/data/data_sources/interface_network_user_data_source.dart';
 import 'package:uwords/features/auth/domain/user_auth_dto.dart';
 import 'package:uwords/features/database/data_sources/savable_user_data_source.dart';
