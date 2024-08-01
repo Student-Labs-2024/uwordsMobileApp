5a2814a6700a43ff79cb58e0c24af3a1d74525fe
Nailloon <artemfilyakin5@gmail.com>
2024-08-02 04:27:33 +0600

    [feat] Added 3 sort methods

--- lib/features/learn/domain/models/subtopic_model.dart
+++ lib/features/learn/domain/models/subtopic_model.dart
@@ -13,4 +13,22 @@ class Subtopic {
   final int progress;
   final String pictureLink;
   final List<WordInfo> wordInfoList;
+
+  DateTime getLatestStudyDate() {
+    if (wordInfoList.isEmpty) {
+      return DateTime.now();
+    }
+    return wordInfoList
+        .reduce((a, b) => a.latestStudy!.isAfter(b.latestStudy!) ? a : b)
+        .latestStudy!;
+  }
 }
+
+Comparator<Subtopic> wordCountComparator =
+    (a, b) => a.wordCount.compareTo(b.wordCount);
+
+Comparator<Subtopic> progressComparator =
+    (a, b) => a.progress.compareTo(b.progress);
+
+Comparator<Subtopic> latestStudyDateComparator =
+    (a, b) => a.getLatestStudyDate().compareTo(b.getLatestStudyDate());
